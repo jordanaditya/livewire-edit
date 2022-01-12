e46df6640347cca9faf859ba1b764b5ea3a74e6b
jordanaditya <jordanbrilliant27@gmail.com>
2022-01-12 09:03:12 +0700

    master

--- app/Http/Livewire/StudentsComponent.php
+++ app/Http/Livewire/StudentsComponent.php
@@ -23,37 +23,6 @@ class StudentsComponent extends Component
         ]);
     }
 
-
-    public function storeStudentData()
-    {
-        //on form submit validation
-        $this->validate([
-            'student_id' => 'required|unique:students', //students = table name
-            'name' => 'required',
-            'email' => 'required|email',
-            'phone' => 'required|numeric',
-        ]);
-
-        //Add Student Data
-        $student = new Students();
-        $student->student_id = $this->student_id;
-        $student->name = $this->name;
-        $student->email = $this->email;
-        $student->phone = $this->phone;
-
-        $student->save();
-
-        session()->flash('message', 'New student has been added successfully');
-
-        $this->student_id = '';
-        $this->name = '';
-        $this->email = '';
-        $this->phone = '';
-
-        //For hide modal after add student success
-        $this->dispatchBrowserEvent('close-modal');
-    }
-
     public function resetInputs()
     {
         $this->student_id = '';
@@ -105,50 +74,12 @@ class StudentsComponent extends Component
         $this->dispatchBrowserEvent('close-modal');
     }
 
-    //Delete Confirmation
-    public function deleteConfirmation($id)
-    {
-        $this->student_delete_id = $id; //student id
-
-        $this->dispatchBrowserEvent('show-delete-confirmation-modal');
-    }
-
-    public function deleteStudentData()
-    {
-        $student = Students::where('id', $this->student_delete_id)->first();
-        $student->delete();
-
-        session()->flash('message', 'Student has been deleted successfully');
-
-        $this->dispatchBrowserEvent('close-modal');
-
-        $this->student_delete_id = '';
-    }
 
     public function cancel()
     {
         $this->student_delete_id = '';
     }
 
-    public function viewStudentDetails($id)
-    {
-        $student = Students::where('id', $id)->first();
-
-        $this->view_student_id = $student->student_id;
-        $this->view_student_name = $student->name;
-        $this->view_student_email = $student->email;
-        $this->view_student_phone = $student->phone;
-
-        $this->dispatchBrowserEvent('show-view-student-modal');
-    }
-
-    public function closeViewStudentModal()
-    {
-        $this->view_student_id = '';
-        $this->view_student_name = '';
-        $this->view_student_email = '';
-        $this->view_student_phone = '';
-    }
 
     public function render()
     {
